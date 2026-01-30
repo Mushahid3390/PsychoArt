3841c087f267bea5c8f7e9af0335f3285467fd77
Mushahid3390 <raza.mushahid@rwit.io>
2026-01-30 18:07:27 +0530

    Resolved a conflict in asked-questions section

--- src/components/sections/TopCreator.jsx
+++ src/components/sections/TopCreator.jsx
@@ -32,7 +32,7 @@ const topCreatorContents = {
 
 const TopCreator = () => {
  return (
-    <div className="w-full h-screen flex flex-col mt-50">
+    <div className="w-full flex flex-col mt-50">
       <div className="text-center w-full flex flex-col items-center">
         <h2 className="font-bold text-[40px] text-white">Top Creator</h2>
         <p className="max-w-105.75 font-medium text-[20px] text-[#FFFFFF80] leading-[120%] mt-2.5">
